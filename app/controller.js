04723629f6a805ebfecfb1d9bff57292be644ea7
kacperadach <kacperadach@gmail.com>
2016-01-22 20:09:28 -0500

    working on api call

--- app/controller.js
+++ app/controller.js
@@ -1,9 +1,10 @@
-angular.module('PlayerScoresApp.controllers', []).controller('playersController', function($scope, playerscoresAPIservice) {
+angular.module('PlayerScoresApp.controllers', []).
+	controller('playersController', function($scope, playerscoresAPIservice) {
 	
 	$scope.nameFilter = null;
 	$scope.playersList = [];
 
 	playerscoresAPIservice.getPlayers().success(function (response) {
-		$scope.playersList = response[0];
+		$scope.playersList = response;
 	});
 });
--- app/services.js
+++ app/services.js
@@ -1,11 +1,12 @@
 angular.module('PlayerScoresApp.services', []).factory('playerscoresAPIservice', function($http) {
 
    	var playerscoresAPI = {};
+   
 
    	playerscoresAPI.getPlayers = function() {
    		return $http({
    			method: 'JSONP',
-   			url: '0.0.0.0:8001/QB/?format=json'
+   			url: 'http://0.0.0.0:8001/QB/?format=json'
    		});
    	}
    
