c8de0c13a90afddbd0eb4ae39ca61150414b3981
kacperadach <kacperadach@gmail.com>
2016-01-22 20:10:43 -0500

    deleting unneeded file

--- app/api.js
+++
@@ -1,6 +0,0 @@
-function Hello($scope, $http) {
-    $http.get('http://0.0.0.0:8001/QB/?format=json').
-        success(function(data) {
-            $scope.quarterbacks = data.data;
-        });
-}
