9abd6766b5911c56e21a365cae2127fa2663f339
kacperadach <kacperadach@gmail.com>
2016-01-29 20:57:12 -0500

    added a filter (doesnt work w/ pagination)

--- app/controller.js
+++ app/controller.js
@@ -1,12 +1,19 @@
 angular.module('PlayerScoresApp.controllers', []).
 	controller('playersController', function($scope, playerscoresAPIservice) {
 	
+	$scope.nameFilter = null;
 	$scope.currentPage = 0;
 	$scope.pageSize = 32;
 	$scope.playersList = [];
+
+	$scope.searchFilter = function (player) {
+		var keyword = new RegExp($scope.nameFilter, 'i');
+		return !$scope.nameFilter || keyword.test(player.name); 
+	};
+
 	$scope.numberOfPages = function ()  {
 		return Math.ceil($scope.playersList.length/$scope.pageSize);
-	}
+	};
 
 	playerscoresAPIservice.getPlayers().then(
 		function (response) {
